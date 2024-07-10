484b920e4eb0905ead81c25d3cd75d577ea4dfca
albim72 <marcin.albiniak72@gmail.com>
2024-07-10 12:29:55 +0200

    Update generatory.py

--- DZIEN_1/generatory.py
+++ DZIEN_1/generatory.py
@@ -9,3 +9,37 @@ print(list(liczby()))
 
 for p in liczby():
     print(p)
+
+
+#przykład 2
+def wznowienie(n,k):
+    print("wstrzymianie działanie")
+    yield 1005
+    print("wznowienie działania")
+
+    print("wstrzymianie działanie")
+    yield n+k
+    print("wznowienie działania")
+
+    n = 8*k - 2
+    print("wstrzymianie działanie")
+    yield n-k
+    print("wznowienie działania")
+
+    print("wstrzymianie działanie")
+    yield n*k
+    print("wznowienie działania")
+
+    print("wstrzymianie działanie")
+    yield n/k
+    print("wznowienie działania")
+
+
+print(wznowienie(6,8))
+print(list(wznowienie(6,8)))
+
+print("_"*50)
+for i in wznowienie(6,8):
+    print("*"*40)
+    print(type(i))
+    print(f'zwrócowno wartośc {i}')
