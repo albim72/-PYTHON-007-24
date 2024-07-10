7ca3d9b83d414a9f6720ea62dee0a33d9d2e9edb
albim72 <marcin.albiniak72@gmail.com>
2024-07-10 14:07:27 +0200

    Update odlresistor.py

--- DZIEN_1/RESISTOR_PROJECT/odlresistor.py
+++ DZIEN_1/RESISTOR_PROJECT/odlresistor.py
@@ -1,9 +1,16 @@
 class OldResistor:
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(OldResistor)
+
     def __init__(self,ohms):
         self._ohms = ohms
-        
+
+    def __repr__(self):
+        return f'To jest obiekt klasy {self.__class__.__name__}'
+
+
     def get_ohms(self):
         return self._ohms
-    
+
     def set_ohms(self,newohms):
         self._ohms = newohms
