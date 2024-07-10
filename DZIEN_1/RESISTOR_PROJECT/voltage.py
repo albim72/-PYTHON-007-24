12dd7750fcdda5561e95c4ad839cc1c131351bf2
albim72 <marcin.albiniak72@gmail.com>
2024-07-10 14:30:22 +0200

    Update voltage.py

--- DZIEN_1/RESISTOR_PROJECT/voltage.py
+++ DZIEN_1/RESISTOR_PROJECT/voltage.py
@@ -4,11 +4,12 @@ class VoltageResistance(Resistor):
     def __init__(self, ohms):
         super().__init__(ohms)
         self._voltage = 0
-        
+
     @property
     def voltage(self):
         return self._voltage
-    
+
     @voltage.setter
     def voltage(self,voltage):
         self._voltage = voltage
+        self.current = self.voltage/self.ohms
