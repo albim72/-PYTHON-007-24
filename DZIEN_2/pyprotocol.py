b9a18ff25ab1ec169c525307d2256156290174bb
albim72 <marcin.albiniak72@gmail.com>
2024-07-11 12:06:47 +0200

    Update pyprotocol.py

--- DZIEN_2/pyprotocol.py
+++ DZIEN_2/pyprotocol.py
@@ -4,14 +4,20 @@ class Item(Protocol):
     quantity:float
     price:float
 
+    def total_price(self)->float:
+        ...
+
 class Product:
     def __init__(self,name:str,quantity:float,price:float):
         self.name = name
         self.quantity = quantity
         self.price = price
+    def total_price(self)->float:
+        return self.quantity*self.price
+
 
 def calculate_total(items:List[Item])->float:
-    return sum([item.quantity*item.price for item in items])
+    return sum([item.total_price() for item in items])
 
 total = calculate_total([
     Product('A',10,24.5),
@@ -22,4 +28,3 @@ total = calculate_total([
 ])
 
 print(total)
-        
