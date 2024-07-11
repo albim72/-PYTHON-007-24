7348789a643913cec81c151050adb38fbbc68978
albim72 <marcin.albiniak72@gmail.com>
2024-07-11 09:32:07 +0200

    Update figura.py

--- DZIEN_1/FIGURY/figura.py
+++ DZIEN_1/FIGURY/figura.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
 
 class Figura(ABC):
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
-        
+    # def __init__(self,a,b):
+    #     self.a=a
+    #     self.b=b
+
+    def __init__(self, a: float, b: float = None) -> None:
+        self.a = a
+        if b:
+            self.b = b
+
     @abstractmethod
     def policz_pole(self):
         pass
