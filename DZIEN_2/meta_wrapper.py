5acc06241fb70f52c15da044570ecb2144673381
albim72 <marcin.albiniak72@gmail.com>
2024-07-12 09:19:00 +0200

    Update meta_wrapper.py

--- DZIEN_2/meta_wrapper.py
+++ DZIEN_2/meta_wrapper.py
@@ -20,9 +20,41 @@ class DebugMeta(type):
         obj = super().__new__(cls, clsname,bases,attrs)
         obj = debugmethods(obj)
         return obj
-    
+
     def __init__(cls, clsname,bases,attrs):
         cls.fc = cls.fc
-        
+
     def fc(cls):
         return "ważna informacja"
+
+
+class Base(metaclass=DebugMeta):pass
+
+class Calc(Base):
+    def add(self,x,y):
+        return (x+y)*100
+
+    def mul(self,x,y):
+        return 0.5*x*y
+
+    def div(self,x,y):
+        return x/(y+10)
+
+    @classmethod
+    def licz(cls,a,b):
+        return a-2*b
+
+    @staticmethod
+    def costam(self,i):
+        return i*90
+
+mc = Calc()
+print(mc.add(4,7))
+print(mc.mul(4,7))
+print(mc.div(4,7))
+print(mc.licz(4,7))
+print(mc.costam(56))
+
+print(mc.fc())
+
+print(Calc.fc())
